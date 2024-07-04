b635c6be1b2b5fb91c1d25c3b791724294ac154a
Seberestmain <berest.alescha@gmail.com>
2024-07-04 13:15:22 +0300

    Сделалано решение для задачи

--- PythSolve.py
+++ PythSolve.py
@@ -1,6 +1,19 @@
+import pandas as pd 
+import numpy as np 
 import random
+ 
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
-data.head()
+data = pd.DataFrame({'whoAmI': lst})
+print(data) # печатаем до
+
+#==================================================#
+print('После скрипта')
+ 
+data['tmp'] = 1  # Временный столбик для значений
+data.set_index([data.index, 'whoAmI'], inplace=True)  #Устанавливается новый индекс DataFrame, состоящий из текущего индекса и значений столбца whoAmI
+data = data.unstack(level=-1, fill_value = 0).astype(int)  # Функция Unstack преобразует уровни индекса в столбцы|Аргумент level=-1 указывает, что нужно "развернуть" последний уровень индекса
+data.columns = data.columns.droplevel() # Убирается уровень столбцов, который появился после unstack
+data.columns.name = None # Удаляется имя
+print(data) #  Печатаем после
